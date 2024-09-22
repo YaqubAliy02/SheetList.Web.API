7588445e57770650c3c10bcc9c7560332a64bebb
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-09-22 16:32:04 +0500

    ShouldAddEmployeeAsync -> PASS

---
+++ SheetList.Web.API.Unit.Test/Services/Foundations/Employees/EmployeeServiceTest.Logic.Add.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using SheetList.Web.API.Models;
+
+namespace SheetList.Web.API.Unit.Test.Services.Foundations.Employees
+{
+    public partial class EmployeeServiceTest
+    {
+        [Fact]
+        public async Task ShouldAddEmployeeAsync()
+        {
+            //given
+            Employee randomEmployee = CreateRandomEmployee();
+            Employee inputEmployee = randomEmployee;
+            Employee storageEmployee = inputEmployee;
+            Employee expectedStorageEmployee = storageEmployee.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertEmployeeAsync(inputEmployee)).ReturnsAsync(storageEmployee);
+
+            //when
+            Employee actualEmployee =
+                await this.employeeServiceMock.AddEmployeeAsync(inputEmployee);
+
+            //then
+            actualEmployee.Should().BeEquivalentTo(expectedStorageEmployee);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertEmployeeAsync(It.IsAny<Employee>()), Times.Once());
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ SheetList.Web.API.Unit.Test/Services/Foundations/Employees/EmployeeServiceTest.cs
@@ -0,0 +1,36 @@
+﻿using Moq;
+using SheetList.Web.API.Brokers.Storages;
+using SheetList.Web.API.Models;
+using SheetList.Web.API.Services.Foundations.Employees;
+using Tynamix.ObjectFiller;
+
+namespace SheetList.Web.API.Unit.Test.Services.Foundations.Employees
+{
+    public partial class EmployeeServiceTest
+    {
+        public readonly Mock<IStorageBroker> storageBrokerMock;
+        public readonly IEmployeeService employeeServiceMock;
+
+        public EmployeeServiceTest()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+
+            this.employeeServiceMock =
+                new EmployeeService(this.storageBrokerMock.Object);
+        }
+
+        private static Employee CreateRandomEmployee() =>
+            CreateEmployeeFiller(date: GetRandomDateTimeOffset()).Create();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Filler<Employee> CreateEmployeeFiller(DateTimeOffset date)
+        {
+            var filler = new Filler<Employee>();
+            filler.Setup().OnType<DateTimeOffset>().Use(date);
+
+            return filler;
+        }
+    }
+}
