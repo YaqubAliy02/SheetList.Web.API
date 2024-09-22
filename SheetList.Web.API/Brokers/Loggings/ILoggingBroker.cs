31ab1dcce0f99d6990d8bac6ac71305fc0a67034
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-09-22 13:20:34 +0500

    BROKERS: Add Logging

---
+++ SheetList.Web.API/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,12 @@
+﻿namespace SheetList.Web.API.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogInformation(string message);
+        void LogDebug(string message);
+        void LogError(string message);
+        void LogWarning(string message);
+        void LogTrace(string message);
+        void LogCritical(Exception exception);
+    }
+}
