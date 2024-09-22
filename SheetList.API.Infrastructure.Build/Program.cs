5ff2f8251a245672bbe6d2144c6ed07e30a092df
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-09-22 15:07:08 +0500

    DATA: Implement Build Project

--- SheetList.API.Infrastructure.Build/Program.cs
+++ SheetList.API.Infrastructure.Build/Program.cs
@@ -1,10 +1,85 @@
-﻿namespace SheetList.API.Infrastructure.Build
+﻿using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+
+namespace SheetList.API.Infrastructure.Build
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var githubPipeline = new GithubPipeline
+            {
+                Name = "SheetList Build Pipeline",
+
+                OnEvents = new Events
+                {
+                    Push = new PushEvent
+                    {
+                        Branches = new string[] { "master" }
+                    },
+
+                    PullRequest = new PullRequestEvent
+                    {
+                        Branches = new string[] { "master" }
+                    }
+                },
+
+                Jobs = new Dictionary<string, Job>
+                {
+                    {
+                        "build",
+
+                        new Job
+                        {
+                            RunsOn = BuildMachines.Windows2022,
+
+                            Steps = new List<GithubTask>
+                            {
+                                new CheckoutTaskV2
+                                {
+                                    Name = "Checking Out Code"
+                                },
+
+                                new SetupDotNetTaskV1
+                                {
+                                    Name = "Setting Up .NET",
+
+                                    TargetDotNetVersion = new TargetDotNetVersion
+                                    {
+                                        DotNetVersion = "8.0.204"
+                                    }
+                                },
+
+                                new RestoreTask
+                                {
+                                    Name = "Restoring Nuget Packages"
+                                },
+
+                                new DotNetBuildTask
+                                {
+                                    Name = "Building Project"
+                                },
+
+                                new TestTask
+                                {
+                                    Name = "Running Tests"
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            string buildScriptPath = "../../../../.github/workflows/dotnet.yml";
+            string directoryPath = Path.GetDirectoryName(buildScriptPath);
+
+            if (!Directory.Exists(directoryPath))
+                Directory.CreateDirectory(directoryPath);
+
+            ADotNetClient aDotNetClient = new ADotNetClient();
+            aDotNetClient.SerializeAndWriteToFile(githubPipeline, path: buildScriptPath);
         }
     }
 }
