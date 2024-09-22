b6cb00de305b1dd23b3c6a421b4f7980d3bdfe57
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-09-22 17:42:22 +0500

    SERVICES: Add Orchestrations Service

---
+++ SheetList.Web.API/Services/Orchestrations/Employees/IEmployeeOrchestrationService.cs
@@ -0,0 +1,12 @@
+﻿using SheetList.Web.API.Models.Orchestrations.Files;
+using SheetList.Web.API.Models;
+
+namespace SheetList.Web.API.Services.Orchestrations.Employees
+{
+    public interface IEmployeeOrchestrationService
+    {
+        Task<IQueryable<Employee>> RetrieveEmployeeAscendingOrderAsync(string orderby);
+        Task<int> ImportExternalFileToTable(IFormFile postedFile);
+        FileConfiguration ConvertSqlDataToXmlFile();
+    }
+}
