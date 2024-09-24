512f96b9957ee50ecfc5677711ee8b607c344e22
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-09-24 11:35:42 +0500

    CONTROLLERS: Add FileDataController And Implement GetAllFileDatas Action

---
+++ SheetList.Web.API/Controllers/FileDataController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SheetList.Web.API.Models;
+using SheetList.Web.API.Services.Foundations.FileDatas;
+
+namespace SheetList.Web.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FileDataController : ControllerBase
+    {
+        private readonly IFileDataService fileDataService;
+
+        public FileDataController(IFileDataService fileDataService)
+        {
+            this.fileDataService = fileDataService;
+        }
+
+        [HttpGet("[action]")]
+        public async ValueTask<ActionResult<IQueryable<FileData>>> GetAllFileDatas()
+        {
+            IQueryable<FileData> gettingAllFileDatas = await this.fileDataService.GetAllFileDataAsync();
+
+            return Ok(gettingAllFileDatas);
+        }
+    }
+}
